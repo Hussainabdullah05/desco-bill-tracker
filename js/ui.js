3fddf98d154f304668375065a9601c864c35cb76
Hussainabdullah05 <128453187+Hussainabdullah05@users.noreply.github.com>
2026-08-01 05:14:58 +0600

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,18 @@
+function updateUI(bill) {
 
+    document.getElementById("totalBill").textContent =
+        bill.totalBill.toFixed(2);
+
+    document.getElementById("energyCharge").textContent =
+        "৳ " + bill.energyCharge.toFixed(2);
+
+    document.getElementById("vat").textContent =
+        "৳ " + bill.vat.toFixed(2);
+
+    document.getElementById("meterRent").textContent =
+        "৳ " + bill.meterRent.toFixed(2);
+
+    document.getElementById("otherCharges").textContent =
+        "৳ " + bill.otherCharges.toFixed(2);
+
+}
