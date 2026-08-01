0f77e7818a72692e1e78d967cf713835463a6fa2
Hussainabdullah05 <128453187+Hussainabdullah05@users.noreply.github.com>
2026-08-01 07:00:57 +0600

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -18,36 +18,91 @@ async function loadSettingsPage(){
     .slice(1)
     .forEach(row=>{
 
-
         currentSettings[row[0]]
         =
         Number(row[1]);
 
-
     });
-
 
 
 
     document
     .getElementById("vatInput")
     .value =
-    currentSettings["VAT"] || 0;
+    currentSettings["VAT"];
 
 
 
     document
     .getElementById("meterRentInput")
     .value =
-    currentSettings["Meter Rent"] || 0;
+    currentSettings["Meter Rent"];
 
 
 
     document
     .getElementById("otherChargesInput")
     .value =
-    currentSettings["Other Charges"] || 0;
+    currentSettings["Other Charges"];
 
+
+
+
+
+    document
+    .getElementById("saveSettingsBtn")
+    .onclick =
+    saveSettings;
+
+
+
+}
+
+
+
+
+async function saveSettings(){
+
+
+    const data={
+
+
+        vat:
+        Number(
+        document.getElementById("vatInput").value
+        ),
+
+
+        meterRent:
+        Number(
+        document.getElementById("meterRentInput").value
+        ),
+
+
+        otherCharges:
+        Number(
+        document.getElementById("otherChargesInput").value
+        )
+
+
+    };
+
+
+
+
+    const result =
+    await updateSettings(data);
+
+
+
+    if(result.success){
+
+        alert(
+        "Settings updated"
+        );
+
+
+    }
 
 
 }
