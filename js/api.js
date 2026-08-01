39b3a83cebb108415570154b7a8b0d3d6bb3f377
Hussainabdullah05 <128453187+Hussainabdullah05@users.noreply.github.com>
2026-08-01 06:02:14 +0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,29 +1,99 @@
-
 const API_URL =
 "https://script.google.com/macros/s/AKfycbzua5pEjkvDL7oyvQPHaqTvqBPaEhMmInQPraLbgxKq5MVxWIM-iKL2swuWYeFRdT1X/exec";
 
-async function getTariffs() {
-    const response = await fetch(`${API_URL}?action=tariffs`);
+
+
+async function getTariffs(){
+
+    const response =
+    await fetch(
+        `${API_URL}?action=tariffs`
+    );
+
     return await response.json();
+
 }
 
-async function getSettings() {
-    const response = await fetch(`${API_URL}?action=settings`);
+
+
+async function getSettings(){
+
+    const response =
+    await fetch(
+        `${API_URL}?action=settings`
+    );
+
     return await response.json();
+
 }
 
-async function getBills() {
-    const response = await fetch(`${API_URL}?action=bills`);
+
+
+
+async function saveBill(data){
+
+
+    data.action="saveBill";
+
+
+    const response =
+    await fetch(
+        API_URL,
+        {
+            method:"POST",
+            body:JSON.stringify(data)
+        }
+    );
+
+
     return await response.json();
+
+
 }
 
-async function saveBill(data) {
 
-    const response = await fetch(API_URL,{
-        method:"POST",
-        body:JSON.stringify(data)
-    });
+
+
+
+
+async function getBills(){
+
+
+    const response =
+    await fetch(
+        `${API_URL}?action=bills`
+    );
+
 
     return await response.json();
+
+
+}
+
+
+
+
+
+
+async function deleteBill(month){
+
+
+    const response =
+    await fetch(
+        API_URL,
+        {
+            method:"POST",
+            body:JSON.stringify({
+
+                action:"deleteBill",
+                month:month
+
+            })
+        }
+    );
+
+
+    return await response.json();
+
 
 }
