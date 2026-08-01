0e21182fbe71bfbd17b0a6243ae2951b1c22f68d
Hussainabdullah05 <128453187+Hussainabdullah05@users.noreply.github.com>
2026-08-01 06:40:25 +0600

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,1 +1,106 @@
+let usageChart;
+let billChart;
 
+
+
+async function loadCharts(){
+
+
+    const bills =
+    await getBills();
+
+
+
+    if(bills.length <= 1)
+    return;
+
+
+
+    const data =
+    bills.slice(1);
+
+
+
+    const months =
+    data.map(row=>row[0]);
+
+
+
+    const units =
+    data.map(row=>Number(row[1]));
+
+
+
+    const billsAmount =
+    data.map(row=>Number(row[18]));
+
+
+
+
+    createUsageChart(
+        months,
+        units
+    );
+
+
+
+    createBillChart(
+        months,
+        billsAmount
+    );
+
+
+}
+
+
+
+
+
+
+function createUsageChart(labels,data){
+
+
+    const ctx =
+    document
+    .getElementById("usageChart");
+
+
+    if(usageChart)
+    usageChart.destroy();
+
+
+
+    usageChart =
+    new Chart(
+        ctx,
+        {
+
+        type:"line",
+
+        data:{
+
+            labels:labels,
+
+            datasets:[{
+
+                label:"kWh Usage",
+
+                data:data,
+
+                tension:0.3
+
+            }]
+
+        },
+
+        options:{
+
+            responsive:true
+
+        }
+
+
+    });
+
+
+}
