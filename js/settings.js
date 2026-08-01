88d005b1af04cfcb992061f24d1537b51b41c377
Hussainabdullah05 <128453187+Hussainabdullah05@users.noreply.github.com>
2026-08-01 06:54:29 +0600

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,53 @@
+let currentSettings = {};
 
+
+
+async function loadSettingsPage(){
+
+
+    const data =
+    await getSettings();
+
+
+
+    currentSettings={};
+
+
+
+    data
+    .slice(1)
+    .forEach(row=>{
+
+
+        currentSettings[row[0]]
+        =
+        Number(row[1]);
+
+
+    });
+
+
+
+
+    document
+    .getElementById("vatInput")
+    .value =
+    currentSettings["VAT"] || 0;
+
+
+
+    document
+    .getElementById("meterRentInput")
+    .value =
+    currentSettings["Meter Rent"] || 0;
+
+
+
+    document
+    .getElementById("otherChargesInput")
+    .value =
+    currentSettings["Other Charges"] || 0;
+
+
+
+}
